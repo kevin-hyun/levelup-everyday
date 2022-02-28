22cdf1b069a9139ecadf5e95944a7e5fd6ed36dc
hyubee <hyubee@hyubees-MacBook-Pro.local>
2022-02-28 16:16:47 +0900

    feat:apply logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './common/logger.middleware';
 
 @Module({
   imports: [
@@ -15,4 +17,8 @@ import { ConfigModule } from '@nestjs/config';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
