fdc77eea333cb1d09bc0b8a21d332c5e0b3af640
hyubee <hyubee@hyubees-MacBook-Pro.local>
2022-03-01 00:11:25 +0900

    feat: apply exception filter , change directory of middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { UsersModule } from './users/users.module';
 import { Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { MiddlewareConsumer } from '@nestjs/common';
-import { LoggerMiddleware } from './common/logger.middleware';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { LoggerMiddleware } from './common/logger.middleware';
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Levelup-everyday')
+    .setDescription('')
+    .setVersion('1.0.0')
+    .build();
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  const PORT = process.env.PORT;
+  await app.listen(PORT);
 }
+
 bootstrap();
